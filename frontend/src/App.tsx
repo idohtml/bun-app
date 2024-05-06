2112026bbe6418c399c1bf55dffb370cd1111703
idohtml <jacques.kanu@gmail.com>
2024-05-06 17:46:48 +0200

    added the api as it's own lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { useEffect, useState } from "react";
-import { hc } from "hono/client";
-import { type ApiRoutes } from "../../server/app";
-
-const client = hc<ApiRoutes>("/");
+import { api } from "./lib/api";
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
 
   useEffect(() => {
     async function fetchTotal() {
-      const res = await client.api.expenses["total-spent"].$get();
+      const res = await api.expenses["total-spent"].$get();
       const data = await res.json();
       setTotalSpent(data.total);
     }
